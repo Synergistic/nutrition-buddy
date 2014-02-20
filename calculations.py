0f8660e50b4362a38f971475a39c035820dcea5a
Synergistic <dfernandez226@gmail.com>
2014-02-20 01:53:41 +0000

    Clean Up & Clarify
    Renamed some local variables for clarity. Removed decimal module.
    Streamlined the energy_needs function when dealing with penn state and fixed
    the issue of it ignoring stress/activity factor with Mifflin.

--- calculations.py
+++ calculations.py
@@ -1,19 +1,16 @@
 import calc.convert as convert
 import calc.anthropometrics as anthro
 import calc.nutrientneeds as nutcalc
-from decimal import Decimal
 
-
-
-def calculations(height, height_unit, weight, weight_unit, age, sex):
+def initial_data(height, height_unit, weight, weight_unit, age, sex):
     '''Method to compute all necessary information and convert'''
     d = {}
     #convert to opposite units
-    anthropometrics = conversions(height, height_unit, weight, weight_unit)
-    d['cm'] = anthropometrics[0]
-    d['kg'] = anthropometrics[1]
-    d['in'] = anthropometrics[2]
-    d['lbs'] = anthropometrics[3]
+    weights_and_heights = conversions(height, height_unit, weight, weight_unit)
+    d['cm'] = weights_and_heights[0]
+    d['kg'] = weights_and_heights[1]
+    d['in'] = weights_and_heights[2]
+    d['lbs'] = weights_and_heights[3]
     d['age'] = int(age)
     d['sex'] = sex.lower()
     
@@ -29,38 +26,35 @@ def calculations(height, height_unit, weight, weight_unit, age, sex):
     if d['%ibw'] >= 125.0:
 	d['abw'] = anthro.adjust_body_weight( d['ibw_kg'], d['kg'] )
     else:
-	d['abw'] = None
+	d['abw'] = 'N/A'
     return d
   
 def energy_needs(d, args):
     #determine energy needs using mifflin
     needs = nutcalc.mifflin(d['kg'], d['cm'], d['sex'], d['age'])
-    print args[0]
-    if 'Penn' in args[0]: #if pennstate is selected, use above value to determine pennstate
-	print 'unit', args[2]
-	print 'temp', args[1]
-	if args[2] == 'F':
-	    tmax = convert.to_celcius(args[1])
-	else:
-	    tmax = args[1]
-	print 'vent', args[3]
+    
+    if 'Penn' in args[0]:
+	if args[2] == 'F': tmax = convert.to_celcius(args[1])
+	else: tmax = args[1]
+	
 	needs = nutcalc.pennstate(needs, d['bmi'], d['sex'], d['age'], tmax, vent_rate=args[3])
+    else:needs *= convert.to_decimal(args[1])
     return needs
       
 def conversions(ht_value, start_ht_unit, wt_value, start_wt_unit):
     
     if start_ht_unit == 'cm': #convert to inches
-        metric = [Decimal(ht_value)]
+        metric = [convert.to_decimal(ht_value)]
 	imperial = [convert.to_inches(ht_value)]    
     elif start_ht_unit == 'in': #convert to centimetres
-        imperial = [Decimal(ht_value)]
+        imperial = [convert.to_decimal(ht_value)]
 	metric = [convert.to_centimeters(ht_value)]
 
     if start_wt_unit == 'kg': #convert to pounds
-	metric.append(Decimal(wt_value))
+	metric.append(convert.to_decimal(wt_value))
 	imperial.append(convert.to_pounds(wt_value))	    
     elif start_wt_unit == 'lbs': #convert to kilograms
-	imperial.append(Decimal(wt_value))
+	imperial.append(convert.to_decimal(wt_value))
 	metric.append(convert.to_kilograms(wt_value))
 
     return metric + imperial
