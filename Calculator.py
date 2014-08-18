9ffaf8ce1b2a426e9e7ca8e7508e01ccf3ed8cfb
Synergistik <DFernandez226@gmail.com>
2014-08-18 18:56:17 -0400

    Spacing Fix
    Removed tabs, made all spaces :)

--- Calculator.py
+++ Calculator.py
@@ -3,12 +3,12 @@ from decimal import Decimal
 
 
 class NutritionCalculator():
-                
+
     def MifflinStJeor(self, height, weight, age, gender):
         '''Caloric needs based on Mifflin-St.Jeor Equation
         Males: (9.99 * weight(kg)) + (6.25 * height(cm)) - (4.92 * age) + 5.0
         Females: (9.99 * weight(kg)) + (6.25 * height(cm)) - (4.92 * age) - 161'''
-        
+
         if gender.isMale:
             caloriesNeeded = ((Decimal('9.99') * weight) + 
                               (Decimal('6.25') * height) - 
@@ -18,22 +18,22 @@ class NutritionCalculator():
                               (Decimal('6.25') * height) -
                               (Decimal('4.92') * age) - Decimal('161.0'))
         return caloriesNeeded
-    
+
     def MifflinFactor(self, mifflinValue, factorToMultiply):
         return Decimal(str(mifflinValue)) * Decimal(factorToMultiply)
-    
+
     def CaloriesPerKilogram(self, calories, weight):
         caloriesPerKilogram = calories / weight
         return caloriesPerKilogram  
-    
+
     def BodyMassIndex(self, weight, height):
         """Calculates Body Mass Index(BMI):
         bmi = (weight(kg) / (height(meters)**2))"""
-        
+
         bmi = weight / ((height / 100) ** 2)
         return bmi
-    
-    
+
+
     def BmiCategory(self, bmi):
 
         if bmi < 18.50:
@@ -49,8 +49,8 @@ class NutritionCalculator():
         elif bmi > 39.99:
             category = "Obese III" 
         return category
-            
-                
+
+
     def IdealBodyWeight(self, heightInches, gender):
         """
         Calculates IBW  in pounds based on Hamwi method;
@@ -60,26 +60,26 @@ class NutritionCalculator():
         """
         if heightInches >= 60:
             inchesOverSixty = heightInches - 60
-            
+
             if gender.isMale:
                 ibw = (inchesOverSixty * 6) + 106
             else:
                 ibw = (inchesOverSixty * 5) + 100
-    
+
         elif heightInches < 60:
             inchesUnderSixty = 60 - heightInches
-            
+
             if gender.isMale:
                 ibw = 106 - (inchesUnderSixty * 3)
             else:
                 ibw = 100 - (inchesUnderSixty * (Decimal('2.5')))
-                
+
         return ibw        
-    
+
     def PercentIdealBodyWeight(self, weight, ibw):
         return (weight / ibw) * Decimal('100.0')
-    
-    
+
+
     def AdjustedBodyWeight(self, weight, ibw):
         return (((weight - ibw) *
-                Decimal('0.25')) + ibw)
+                 Decimal('0.25')) + ibw)
